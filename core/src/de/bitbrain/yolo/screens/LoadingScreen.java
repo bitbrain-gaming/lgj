1878413696159c93a02177062db2e192ce1f7b61
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-01-24 15:18:47 +0100

    Added 2 more screens

---
+++ core/src/de/bitbrain/yolo/screens/LoadingScreen.java
@@ -0,0 +1,32 @@
+package de.bitbrain.yolo.screens;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+import de.bitbrain.yolo.YoloGame;
+
+public class LoadingScreen extends AbstractScreen {
+
+	LoadingScreen(YoloGame game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onShow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onResize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onDraw(Batch batch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
