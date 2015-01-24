77a5338639533997ac9ca991cda5de9c166abcde
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-01-24 20:14:10 +0100

    Added first projectile

--- core/src/de/bitbrain/yolo/behaviors/PlayerBehavior.java
+++ core/src/de/bitbrain/yolo/behaviors/PlayerBehavior.java
@@ -1,37 +1,64 @@
 package de.bitbrain.yolo.behaviors;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Vector2;
 
+import de.bitbrain.yolo.core.GameHandler;
 import de.bitbrain.yolo.core.GameObject;
+import de.bitbrain.yolo.core.GameObjectType;
 
 public class PlayerBehavior implements Behavior {
 	
 	private static final float ACCELERATION_FACTOR = 20.5f;
 	
+	private static final float SHOOT_INTERVAL = 0.5f;
+	
+	private static final float SHOOT_SPEED = 20f;
+	
 	private Vector2 direction = new Vector2();
 	
 	private Camera camera;
 	
-	public PlayerBehavior(Camera camera) {
+	private GameHandler gameHandler;
+	
+	public PlayerBehavior(Camera camera, GameHandler handler) {
 		this.camera = camera;
+		this.gameHandler = handler;
 	}
 
 	@Override
 	public void update(GameObject target, float delta) {
 		if (Gdx.input.isKeyPressed(Keys.W)) {
 			target.getAcceleration().y -= ACCELERATION_FACTOR;
-		} else if (Gdx.input.isKeyPressed(Keys.A)) {
-			target.getAcceleration().x -= ACCELERATION_FACTOR;
 		} else if (Gdx.input.isKeyPressed(Keys.S)) {
 			target.getAcceleration().y += ACCELERATION_FACTOR;
+		}
+		if (Gdx.input.isKeyPressed(Keys.A)) {
+			target.getAcceleration().x -= ACCELERATION_FACTOR;
 		} else if (Gdx.input.isKeyPressed(Keys.D)) {
 			target.getAcceleration().x += ACCELERATION_FACTOR;
 		}
+		
 		direction.x =  target.getPosition().x + camera.viewportWidth / 2f - (Gdx.input.getX() + camera.position.x);
 		direction.y =  target.getPosition().y + camera.viewportHeight / 2f - (Gdx.input.getY() + camera.position.y);
 		target.setAngle(direction.angle() + 90f);
+		if (Gdx.input.isButtonPressed(Buttons.LEFT)) {
+			shoot(target);
+		}
+	}
+	
+	private void shoot(GameObject ship) {
+		GameObject projectile = new GameObject();
+		projectile.setType(GameObjectType.PROJECTILE);
+		projectile.setPosition(ship.getPosition().x, ship.getPosition().y);
+		gameHandler.applyBehavior(projectile, new ProjectileBehavior(gameHandler));
+		projectile.setAngle(direction.angle());
+		projectile.setSize(8f, 8f);
+		direction.nor();
+		projectile.setVelocity(direction.x * SHOOT_SPEED, direction.y * SHOOT_SPEED);
+		gameHandler.addGameObject(projectile);
 	}
 }
--- core/src/de/bitbrain/yolo/core/Physics.java
+++ core/src/de/bitbrain/yolo/core/Physics.java
@@ -7,32 +7,38 @@ public class Physics {
 	public static final float MIN_SPEED = 120f;
 
 	public static final float MAX_SPEED = 350f;
-	
+
 	public static final float MAX_ACCEL = 25f;
 
 	public void apply(GameObject gameObject, float delta) {
-		
-		if (gameObject.getAcceleration().x > MAX_ACCEL) {
-			gameObject.getAcceleration().x = MAX_ACCEL;
-		}
-		if (gameObject.getAcceleration().y > MAX_ACCEL) {
-			gameObject.getAcceleration().y = MAX_ACCEL;
-		}
-		gameObject.getAcceleration().scl(0.4f);
-		
-		if (gameObject.getAcceleration().len() < 5f && gameObject.getVelocity().len() > MIN_SPEED) {
-			gameObject.getVelocity().scl(0.995f);
-		} else if (gameObject.getVelocity().len() < MIN_SPEED) {
-			Vector2 corrected = gameObject.getVelocity().nor();
-			corrected.scl(MIN_SPEED, MIN_SPEED);
-		}
-		
-		gameObject.setVelocity(
-				gameObject.getVelocity().x + gameObject.getAcceleration().x,
-				gameObject.getVelocity().y + gameObject.getAcceleration().y);
-		if (gameObject.getVelocity().len() > MAX_SPEED) {
-			Vector2 corrected = gameObject.getVelocity().nor();
-			corrected.scl(MAX_SPEED, MAX_SPEED);
+
+		if (!gameObject.getType().equals(GameObjectType.PROJECTILE)) {
+			if (gameObject.getAcceleration().x > MAX_ACCEL) {
+				gameObject.getAcceleration().x = MAX_ACCEL;
+			}
+			if (gameObject.getAcceleration().y > MAX_ACCEL) {
+				gameObject.getAcceleration().y = MAX_ACCEL;
+			}
+			gameObject.getAcceleration().scl(0.4f);
+
+			if (gameObject.getAcceleration().len() < 5f
+					&& gameObject.getVelocity().len() > MIN_SPEED) {
+				gameObject.getVelocity().scl(0.995f);
+			} else if (gameObject.getVelocity().len() < MIN_SPEED) {
+				Vector2 corrected = gameObject.getVelocity().nor();
+				corrected.scl(MIN_SPEED, MIN_SPEED);
+			}
+
+			gameObject
+					.setVelocity(
+							gameObject.getVelocity().x
+									+ gameObject.getAcceleration().x,
+							gameObject.getVelocity().y
+									+ gameObject.getAcceleration().y);
+			if (gameObject.getVelocity().len() > MAX_SPEED) {
+				Vector2 corrected = gameObject.getVelocity().nor();
+				corrected.scl(MAX_SPEED, MAX_SPEED);
+			}
 		}
 		gameObject.setPosition(
 				gameObject.getPosition().x
