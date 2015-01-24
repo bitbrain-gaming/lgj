32754002b40d10b90af8a04d032b37a3bea47106
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-01-24 12:59:53 +0100

    Created game structure

---
+++ desktop/src/de/bitbrain/yolo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.bitbrain.yolo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.bitbrain.yolo.YoloGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YoloGame(), config);
+	}
+}
